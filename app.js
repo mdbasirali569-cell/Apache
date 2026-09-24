03c9f13e0685e4217034f7505e57b8bb3b6127bf
mdbasirali569-cell <mdbasirali569@gmail.com>
2026-09-24 22:39:10 +0600

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const items = document.querySelectorAll('.item');
-
-items.forEach(item => {
-  item.addEventListener('click', () => {
-    items.forEach(i => i.setAttribute('aria-selected', 'false'));
-    item.setAttribute('aria-selected', 'true');
-  });
-});
